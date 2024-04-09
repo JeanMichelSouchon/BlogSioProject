d056df50233776fd53c2bc31aa266c1fb0a6b880
Jsouchon <jeanmichelsouchon83170@outlook.fr>
2024-04-09 16:47:55 +0200

    login
    ajout de la fonction login

--- assets/connect.js
+++ assets/connect.js
@@ -1,4 +1,3 @@
-
 // Fonction appelée lors de la soumission du formulaire de connexion
 function login() {
     // Récupération des valeurs du formulaire
@@ -6,14 +5,46 @@ function login() {
     var password = document.getElementById('password').value;
 
     // Vérification des identifiants (c'est un exemple très basique, à remplacer par une vérification côté serveur dans un contexte réel)
-    if (username === 'utilisateur' && password === 'motdepasse') {
+    if (username === 'iziiak' && password === 'espoir15') {
         // Authentification réussie, stockage de l'information de connexion dans le localStorage
         localStorage.setItem('loggedIn', 'true');
         localStorage.setItem('username', username);
 
         // Redirection vers la page principale
         window.location.href = 'index.html';
-    } else {
+    } else if (username === 'gianni' && password === 'root') {
+        // Authentification réussie, stockage de l'information de connexion dans le localStorage
+        localStorage.setItem('loggedIn', 'true');
+        localStorage.setItem('username', username);
+
+        // Redirection vers la page principale
+        window.location.href = 'index.html';
+    }else if (username === 'jules' && password === 'root') {
+        // Authentification réussie, stockage de l'information de connexion dans le localStorage
+        localStorage.setItem('loggedIn', 'true');
+        localStorage.setItem('username', username);
+
+        // Redirection vers la page principale
+        window.
+        location.href = 'index.html';
+    }
+    else if (username === 'tristan' && password === 'root') {
+        // Authentification réussie, stockage de l'information de connexion dans le localStorage
+        localStorage.setItem('loggedIn', 'true');
+        localStorage.setItem('username', username);
+
+        // Redirection vers la page principale
+        window.location.href = 'index.html';
+    }
+    else if (username === 'jm' && password === 'root') {
+        // Authentification réussie, stockage de l'information de connexion dans le localStorage
+        localStorage.setItem('loggedIn', 'true');
+        localStorage.setItem('username', username);
+
+        // Redirection vers la page principale
+        window.location.href = 'index.html';
+    }else
+    {
         // Affichage d'un message d'erreur en cas d'authentification échouée
         alert('Identifiants incorrects');
     }
