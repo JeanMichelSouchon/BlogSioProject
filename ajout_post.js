960455a31686da2e60e74f25d52c5882bc81cb6f
Tristan LINDENBERG <malteronie.titouliden83@hotmail.com>
2024-04-10 09:46:06 +0200

    maj

--- ajout_post.js
+++ ajout_post.js
@@ -5,6 +5,10 @@ if (typeof(Storage) !== "undefined") {
         // Si la todolist n'existe pas encore, on crée une liste vide
         localStorage.setItem("Post", JSON.stringify([]));
     }
+    if (!localStorage.getItem("Nom")) {
+        // Si la todolist n'existe pas encore, on crée une liste vide
+        localStorage.setItem("Nom", JSON.stringify([]));
+    }
 } else {
     // Code pour le stockage local non disponible
     alert("Désolé, votre navigateur ne supporte pas le stockage local.");
@@ -14,6 +18,8 @@ if (typeof(Storage) !== "undefined") {
 function ajouterElement() {
     const inputElement = document.getElementById("post");
     const nouvelElement = inputElement.value;
+    const inputNom = document.getElementById("nom");
+    const nouvelNom = inputNom.value;
     if (nouvelElement.trim() !== "") {
         const todolist = JSON.parse(localStorage.getItem("Post"));
         todolist.push(nouvelElement);
@@ -21,5 +27,16 @@ function ajouterElement() {
         //afficherTodolist();
         // Réinitialise le champ de saisie
         inputElement.value = "";
+    }
+    if (nouvelNom.trim() !== "") {
+        const todolistnom = JSON.parse(localStorage.getItem("Nom"));
+        todolistnom.push(nouvelNom);
+        localStorage.setItem("Nom", JSON.stringify(todolistnom));
+        //afficherTodolist();
+        // Réinitialise le champ de saisie
+        inputNom.value = "";
+    } else {
+        alert("Désolé, votre navigateur ne supporte pas le stockage local.");
+
     }
 }
